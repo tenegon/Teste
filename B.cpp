9f9f9068ff84c4001cb2cb840d0048e6c25f13cf
tenegon <tenegon@hotmail.com>
2017-11-14 13:56:43 -0200

    Este commit é para acrescentar a classe B, que herda de A, e, instanciar um objeto da classe B no main

---
+++ B.cpp
@@ -0,0 +1,12 @@
+#include "A.cpp"
+
+using namespace std;
+
+class B : public A{
+	private:
+		string s;
+	protected:
+	public:
+		B() : A(), s("b"){};
+		void print(){ A::print(); cout << s << endl;}
+};
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
-#include "A.cpp"
+#include "B.cpp"
 
 using namespace std;
 
@@ -9,6 +9,8 @@ int main(int argc, const char * argv[])
 {
 	A *a = new A();
 	a->print();
+	B *b = new B();
+	b->print();
 	return 0;
 }
 
